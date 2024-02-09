8ef9c82a4f0a19ccaf1e3b4e7183871282496d5d
Evgeni-Zarchov <142100154+Evgeni-Zarchov@users.noreply.github.com>
2024-02-09 17:46:10 +0200

    #

---
+++ RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace RefactorSpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            int currentSum = 0;
+
+            bool isTrue = false;
+
+            for (int i = 1; i <= input; i++)
+
+            {
+
+                currentSum = i;
+
+                while (i > 0)
+
+                {
+
+                    sum += i % 10;
+
+                    i = i / 10;
+
+                }
+
+                isTrue = (sum == 5) || (sum == 7) || (sum == 11);
+
+                Console.WriteLine("{0} -> {1}", currentSum, isTrue);
+
+                sum = 0;
+
+                i = currentSum;
+
+            }
+        }
+    }
+}
---
+++ RefactorVolumeOfPyramid/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace RefactorVolumeOfPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Im given a working code but i have to refactor it 
+             //double dul, sh, V = 0;
+            // the variable dul should become length;
+
+            // Console.WriteLine("Length: ");
+
+            // dul = double.Parse(Console.ReadLine());
+
+            // Console.WriteLine("Width: ");
+
+            // sh = double.Parse(Console.ReadLine())
+            // sh should become width;
+
+            // Console.WriteLine("Heigth: ");
+
+            // V = double.Parse(Console.ReadLine());
+            // V = (dul + sh + V) / 3;
+            // Console.WriteLine($"Pyramid Volume: {V:f2}");
+
+            // the formula is length * width * volume / 3;
+
+
+            double length = double.Parse(Console.ReadLine());
+            Console.Write("Length: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            double height = double.Parse(Console.ReadLine());
+
+            height = (length * width * height) / 3;
+
+            Console.Write($"Pyramid Volume: {height:f2}");
+            
+        }
+    }
+}
