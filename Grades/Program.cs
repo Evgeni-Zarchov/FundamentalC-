3d63c07821a94105383d95f2bea85fe8599ea2f1
Evgeni-Zarchov <142100154+Evgeni-Zarchov@users.noreply.github.com>
2024-02-27 20:45:33 +0200

    Methods

---
+++ Grades/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrintGrade(double.Parse(Console.ReadLine()));
+        }
+
+        static void PrintGrade(double grades)
+        {
+            string gradeInWords = string.Empty;
+            if (grades > 2.00 && grades <= 2.99)
+            {
+                gradeInWords = "Fail";
+            }
+            else if (grades >= 3.00 && grades <= 3.49)
+            {
+                gradeInWords = "Poor";
+            }
+            else if (grades >= 3.50 && grades <= 4.49)
+            {
+                gradeInWords = "Good";
+            }
+            else if (grades >= 4.50 && grades <= 5.49)
+            {
+                gradeInWords = "Very good";
+            }
+            else if (grades >= 5.50 && grades <= 6.00)
+            {
+                gradeInWords = "Excellent";
+            }
+            Console.WriteLine(gradeInWords);
+        }
+    }
+}
---
+++ SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SignOfIntegerNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            SignNumbers(numbers);
+        }
+
+        static void SignNumbers(int numbers)
+        {
+            if (numbers > 0 )
+            {
+                Console.WriteLine($"The number {numbers} is positive.");
+            }
+            else if (numbers < 0 )
+            {
+                Console.WriteLine($"The number {numbers} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {numbers} is zero.");
+            }
+        }
+    
+    }
+}
